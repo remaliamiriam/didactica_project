87c59edec69a5931e2af6c327a52e168f1a6e37d
remaliamiriam <aioneseimiriam@gmail.com>
2025-06-11 16:20:04 +0300

    Build frontend with base='./' fix pentru producție

--- didactica-project/frontend/vite.config.js
+++ didactica-project/frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: './',    // important pentru căile relative în build
   plugins: [react()],
   server: {
     port: 3000,
